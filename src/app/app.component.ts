4254fc36cd2d1ef23247e6ba9026009177720c4e
prateekis27 <69340535+prateekis27@users.noreply.github.com>
2020-08-18 12:41:36 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component, VERSION } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  email: string;
+  password: string;
+  router: any;
+  // tslint:disable-next-line: typedef
+  onSubmit(data) {
+    // console.log(data);
+    // console.log(data.email);
+    // console.log(data.password);
+    // tslint:disable-next-line: triple-equals
+    if (data.email == 'prateekis27@gmail.com' && data.password == '1234') {
+      alert('login succesfully');
+      // tslint:disable-next-line: no-unused-expression
+      // this.router.navigate['../app/welcome/welcome.component.html'];
+    } else {
+      alert('Wrong credential');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [AppComponent, WelcomeComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
